6544c8bbd2f975bec7f1fba8ee0e4a46361c15fe
Viswa <viswapranesh8@gmail.com>
2026-02-23 10:21:43 +0530

    your_name Added UsecaseName

--- panlindromeApp/src/PalindromeCheckerApp.java
+++ panlindromeApp/src/PalindromeCheckerApp.java
@@ -1,9 +1,11 @@
-// version 1.0
-//author viswa
-//useCase 1: Welcome Page
+// version 2.0
+//author Viswa
+//useCase 2:
 public class palindromeCheckerApp {
     public static void main(string[] args){
         system.out.println("welcome to Palindrome Checker App Management System")
+        system.out.println("modify this logic to usecase 2");
+
     }
 }
 
